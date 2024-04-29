3d891247cfb231e83ab3e8a4a38e3c348eb970d6
mohiniitgithub <166147919+mohiniitgithub@users.noreply.github.com>
2024-04-30 00:25:58 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const startWatchingBtn = document.querySelector('.hero button');
+
+    startWatchingBtn.addEventListener('click', function () {
+        // You can add functionality here to navigate to a specific page or display a modal
+        console.log('Start Watching button clicked!');
+    });
+});
